78b8f639db67da85068ca9f479409a787351ece2
Matthew Paluch <mattio129@yahoo.com>
2024-01-30 19:23:21 -0500

    actually commit first auto program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest1.java
@@ -1,2 +1,47 @@
-package org.firstinspires.ftc.teamcode;public class AutoTest1 {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "AutoTest3 (Blocks to Java)")
+public class AutoTest3 extends LinearOpMode {
+
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+
+    /**
+     * This function is executed when this OpMode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
+        // Put initialization blocks here.
+        waitForStart();
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (!isStopRequested()) {
+                // Put loop blocks here.
+                backLeftMotor.setPower(0.5);
+                frontLeftMotor.setPower(0.5);
+                frontRightMotor.setPower(0.5);
+                backRightMotor.setPower(0.5);
+                sleep(1000);
+                backLeftMotor.setPower(0);
+                frontLeftMotor.setPower(0);
+                frontRightMotor.setPower(0);
+                backRightMotor.setPower(0);
+                sleep(10000);
+                telemetry.update();
+            }
+        }
+    }
 }
